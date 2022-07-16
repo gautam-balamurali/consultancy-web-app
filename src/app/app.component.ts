603a56d75b37b9a153d12f0e1acaab763dde4e73
Gautam Balamurali <gtmbalamurali@gmail.com>
2022-07-17 02:19:10 +0530

    Hosting and documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,10 @@
+/**
+ * @license
+ * Copyright © 2022, Gautam Balamurali <gtmbalamurali@gmail.com>.
+ * All rights reserved.
+ * This source code is licensed and license can be found in the
+ * LICENSE file in the root directory of this source tree. 
+ */
 import { Component } from '@angular/core';
 
 @Component({
